106364e25425076becd3b43bc77f7bc4f783d93a
vglafirov <vglafirov@gmail.com>
2013-05-03 12:33:35 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gearman-admin',
+      version='0.1',
+      description='Gearman admin command line interface',
+      author='Vladimir Glafirov',
+      author_email='vglafirov@gmail.com',
+      url='https://github.com/vglafirov/gearman-admin',
+      packages=['gearmanadmin'])
